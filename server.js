7d34ac463c94bab56b07f211a0f50416664fa463
Lee Shavit <lee.shavit10@gmail.com>
2024-10-21 16:02:31 +0300

    Limit bugs view to 3

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import { bugService } from './services/bugs.service.js'
 
 const app = express()
@@ -7,6 +8,7 @@ app.listen(3030, () => console.log('Server ready at port 3030'))
 
 //express configuration
 app.use(express.static('public'))
+app.use(cookieParser())
 
 
 //express routing:
@@ -23,7 +25,7 @@ app.get('/api/bug', (req, res) => {
 //save
 app.get('/api/bug/save', (req, res) => {
     const { _id, title, severity, description, createdAt } = req.query
-    const bugToSave = { _id, title, severity: +severity, description, createdAt}
+    const bugToSave = { _id, title, severity: +severity, description, createdAt }
     bugService.save(bugToSave)
         .then(bug => res.send(bug))
         .catch(err => {
@@ -35,8 +37,21 @@ app.get('/api/bug/save', (req, res) => {
 //read item
 app.get('/api/bug/:bugId', (req, res) => {
     const { bugId } = req.params
+    const visitedBugs = req.cookies.visitedBugs || []
     bugService.get(bugId)
-        .then(bug => res.send(bug))
+        .then(bug => {
+            if (visitedBugs.includes(bugId)) {
+                res.send(bug)
+            } else {
+                visitedBugs.push(bugId)
+                res.cookie('visitedBugs', visitedBugs, { maxAge: 1000 * 7 })
+                if (visitedBugs.length > 3) {
+                    res.status(401).send('Wait for a bit')
+                } else {
+                    res.send(bug)
+                }
+            }
+        })
         .catch(err => {
             console.log('err:', err)
             res.status(500).send('Cannot find bug')
